9245fae8e85ab09751217d42df3487ef53da762b
snacks9405 <93486382+snacks9405@users.noreply.github.com>
2022-10-19 19:30:48 -0500

    addTest done

--- AppTest1/UnitTest1.cs
+++ AppTest1/UnitTest1.cs
@@ -8,13 +8,23 @@ namespace AppTest1
         public void Setup()
         {
         }
-
         [Test]
         public void AddTest()
         {
-            String[] Clues = { "CLUE", "CLUE", "CLUE", null };
-            BusinessLogic.AddEntry();
-            Assert.Pass();
+            InvalidFieldError idealResult = InvalidFieldError.NoError;
+            BusinessLogic businessLogic = new();
+
+            String[] clues = { "CLUE", "CLUE", "CLUE", "" };
+            String[] answers = { "answer", "answer", "answer", "" };
+            int[] difficulties = { 0, 1, 3, 2 };
+            String[] dates = { "05/25/1991", "05/35/1991", "05/25/1991", "05/25/1991" };
+            for (int i = 0; i < 4; i++)
+            {
+                InvalidFieldError e = businessLogic.AddEntry(clues[i], answers[i], difficulties[i], dates[i]);
+                Assert.That(e, Is.EqualTo(idealResult));
+            }
+
+
         }
     }
 }
